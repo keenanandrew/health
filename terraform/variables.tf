f354b84e44ff4f37d657045fb43f95ab125e9279
Andrew <keenanandrew@gmail.com>
2024-01-29 18:40:25 +0000

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,70 @@
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "edinburgh-project"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "edinburgh-project-412715"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "europe-west2"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "europe-west2-a"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "edinburgh-project"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "keenanandrew"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  default     = "root"
+  sensitive   = true
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "mageai/mageai:latest"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
